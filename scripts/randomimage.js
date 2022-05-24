273ab77a66d6a9a49233edfa66eaa026254dc346
quissberry <nooblivecam@gmail.com>
2022-05-24 16:47:15 -0700

    Revamped colors, first version of random colors (working)

--- scripts/randomimage.js
+++ scripts/randomimage.js
@@ -1,5 +1,5 @@
 console.log("Hello World!");
-
-function insertRandomImage() {
-  document.getElementById("imageLocation").innerHTML = "<img src=\"" + "randomcolors/starlightcolors.png" + "\" alt=\"A random color\" style = \" display:block;width:100%; height:100%;object-fit: cover;\"><img/";
+function insertRandomImage(x) {
+	const randomPicture = "randomcolors/" + Math.floor(Math.random() * x).toString() + ".png"
+	document.getElementById("imageLocation").innerHTML = "<img src=\"" + randomPicture + "\" alt=\"Hi. If you are able to see this message, I am sorry to tell you that this website has broke in your browser.\" style = \" display:block;width:100%; height:100%;object-fit: cover;\"><img/";
 }

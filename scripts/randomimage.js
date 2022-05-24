ec6dd2a000b02ab20d9dc60ad3c28e52af521aa7
quissberry <nooblivecam@gmail.com>
2022-05-23 18:57:25 -0700

    Random colors displays purple

--- scripts/randomimage.js
+++ scripts/randomimage.js
@@ -1,1 +1,5 @@
- src="scripts/randomimage.js"
+console.log("Hello World!");
+
+function insertRandomImage() {
+  document.getElementById("imageLocation").innerHTML = "<img src=\"" + "randomcolors/starlightcolors.png" + "\" alt=\"A random color\" style = \" display:block;width:100%; height:100%;object-fit: cover;\"><img/";
+}

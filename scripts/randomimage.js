6ecc36f2f2b27659e0d329a79e548548f458360b
quissberry <nooblivecam@gmail.com>
2022-05-26 16:33:17 -0700

    Added randombirbs and fixed randomimage

--- scripts/randomimage.js
+++ scripts/randomimage.js
@@ -3,5 +3,6 @@ function insertRandomImage(totalImages) {
 	const randomPicture = "randomcolors/" + Math.floor(Math.random() * totalImages).toString() + ".png"
 	document.getElementById("imageLocation").style.backgroundImage = "url(" + randomPicture + ")";
 }
-function returnRandomImage()
+function returnRandomImage(){
 	return "https://th.bing.com/th/id/OIP.4we6lgUd-PqAES-ey4nh8QHaJ7?pid=ImgDet&rs=1"
+}

96f7ce917f70b86af931f19cd1463790b0c9d4b3
thairanaru <thairanaru@proton.me>
2024-06-22 15:48:29 -0700

    persistent color schemes

--- scripts/color-scheme.js
+++ scripts/color-scheme.js
@@ -1,4 +1,10 @@
-function switchColorScheme(color-scheme-name) {
-  var element = document.body;
-  element.classList.toggle(color-scheme-name);
-} 
+const colorSchemeSelector = document.getElementById("color-scheme");
+const previousInput = localStorage.getItem("colorScheme");
+console.log(previousInput);
+if (previousInput != null) {
+    colorSchemeSelector.value = previousInput;
+}
+colorSchemeSelector.onchange = (event) => {
+    const inputValue = event.target.value;
+    localStorage.setItem("colorScheme",inputValue)
+}
